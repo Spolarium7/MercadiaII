4141898bb3733ed2372b5633889d5f4cb678d5ba
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-08-04 22:17:56 +0800

    Models

---
+++ GoshenJimenez.MercadiaII.Web/Infrastructure/Data/Enums/LoginStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.MercadiaII.Web.Infrastructure.Data.Enums
+{
+    public enum LoginStatus
+    {
+        Unverified,
+        Active,
+        Locked,
+        NeedsToChangePassword
+    }
+}
---
+++ GoshenJimenez.MercadiaII.Web/Infrastructure/Data/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using GoshenJimenez.MercadiaII.Web.Infrastructure.Data.Enums;
+using GoshenJimenez.MercadiaII.Web.Infrastructure.Data.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.MercadiaII.Web.Infrastructure.Data.Models
+{
+    public class User : BaseModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+
+        public Gender Gender { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginStatus LoginStatus { get; set; }
+
+        public string RegistrationCode { get; set; }
+
+        public int LoginTrials { get; set; }
+    }
+}
