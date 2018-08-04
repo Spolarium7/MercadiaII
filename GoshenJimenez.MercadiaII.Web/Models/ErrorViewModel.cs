0224454dd0ecf5c414157e32c26bd2d559e32665
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-08-04 21:42:59 +0800

    Project Setup

---
+++ GoshenJimenez.MercadiaII.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoshenJimenez.MercadiaII.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
